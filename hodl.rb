a8ec745c231b19e477b6dc653740155210a7b3d0
Ayush Tambde <tambdeayush@gmail.com>
2022-06-13 14:20:17 +0100

    v1.0.0

---
+++ hodl.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Hodl < Formula
+  desc "A Higher-Level Quantum Oracle Description Language (HODL)"
+  homepage "https://github.com/at2005/HODL"
+  url "https://github.com/at2005/HODL/archive/refs/tags/v1.tar.gz"
+  sha256 "fd579f6abb0afbe562691f38407b1301d374983cd518904ae69da3fcbb18cbb7"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    bin.install 'hodl'
+   # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test HODL`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end

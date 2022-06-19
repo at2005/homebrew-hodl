c09de8477943d1fdc16d31eadd274c89b9c8e7eb
Ayush Tambde <tambdeayush@gmail.com>
2022-06-19 21:47:18 +0100

    update

--- hodl.rb
+++ hodl.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Hodl < Formula
   desc "A Higher-Level Quantum Oracle Description Language (HODL)"
   homepage ""
@@ -8,12 +5,10 @@ class Hodl < Formula
   sha256 "fd579f6abb0afbe562691f38407b1301d374983cd518904ae69da3fcbb18cbb7"
   license "MIT"
 
-  # depends_on "cmake" => :build
-
   def install
     mkdir "bin"
     system "make"
-    bin.install "bin/qc"
+    bin.install "bin/hodl"
   end
 
 end

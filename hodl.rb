5a5d67929871209670b086ac89ddbd52367fe839
Ayush Tambde <tambdeayush@gmail.com>
2022-06-19 21:52:04 +0100

    update

--- hodl.rb
+++ hodl.rb
@@ -2,7 +2,7 @@ class Hodl < Formula
   desc "A Higher-Level Quantum Oracle Description Language (HODL)"
   homepage ""
   url "https://github.com/at2005/HODL/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "fd579f6abb0afbe562691f38407b1301d374983cd518904ae69da3fcbb18cbb7"
+  sha256 "5438f90b5df21539a6b15484c2416149a894ac12a5ac5644b6ccddcf06507618"
   license "MIT"
 
   def install
